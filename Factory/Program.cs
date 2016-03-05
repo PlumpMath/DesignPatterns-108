f21284fa3d5d7c18060288f4d3fd2cd9e3e5be1f
benburnsred <agonoy.ben@gmail.com>
2016-03-05 21:44:33 +0800

    Strategy Pattern

---
+++ Factory/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var persons = DataFactory.GetPersons();
+            foreach (var person in persons)
+            {
+                var fullName = string.Format("First Name: {0} \nMiddle Name: {1} \nLast Name: {2} \n",person.FirstName,person.MiddleName, person.LastName);
+                Console.Write(fullName);
+            }
+            Console.ReadKey();
+
+        }
+    }
+
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+    }
+
+    public class DataFactory {
+
+
+        public static IList<Person> GetPersons()
+        {
+            var person = new Person() { };
+            var persons = new List<Person>
+            {
+                new Person  { FirstName = "Antonio", MiddleName = "Vargas", LastName = "Luna" },
+                new Person  { FirstName = "Juan", MiddleName = "Jacinto", LastName = "Luna" },
+            };
+            return persons;
+        }
+    }
+    
+ }
---
+++ Strategy/Main.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strategy
+{
+    class StrategyPattern
+    {
+        static void Main(string[] args)
+        {
+            Animal bruno = new Dog();
+            Animal tisay = new Bird();
+
+            Console.WriteLine(string.Format("Dog: {0}", bruno.flyingType.fly()));
+            Console.WriteLine(string.Format("Bird: {0}", tisay.flyingType.fly()));
+            Console.ReadKey();
+
+            bruno.setFlyingAbility(new ItFlys());
+            Console.WriteLine(string.Format("Dog: {0}", bruno.flyingType.fly()));
+            Console.ReadKey();
+
+        }
+    }
+}
+
+
+//When To Use Strategy Pattern?
+
+//When You want to define a class that will have one behavior that is similar to other behaviors in a list
+
+//When you need to use one of several behaviors dynamically.
+
+//I want the class object to be able to choose from
+//  -Not Flying
+//  -Fly with wings
+//  -Fly super fast
+
